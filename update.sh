a866b49dd3532373b70005bff83983a018e7064c
Julian Groshaupt <mail@julian-groshaupt.de>
2021-02-16 23:19:12 +0100

    updated update.sh * run rcup from home-directory

--- update.sh
+++ update.sh
@@ -4,8 +4,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-# pull repository
+# pull repository & update submodules
 git pull
+git submodule update --init --recursive
 
 # update
+cd $HOME
 rcup
