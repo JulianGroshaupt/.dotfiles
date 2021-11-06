b64827b1f5ed0e6af48bf93a83825a33d2dd1b48
Julian Groshaupt <mail@julian-groshaupt.de>
2021-11-06 12:57:22 +0100

    updated tmux

--- tmux_right.sh
+++ tmux_right.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# check os type
 if [[ $OSTYPE == linux* ]]; then
 	HOSTCOLOR=green
 elif [[ $OSTYPE == darwin* ]]; then
@@ -9,6 +10,7 @@ else
 	HOSTCOLOR=white
 fi
 
+# set output
 OUTPUT=""
 
 # add date
@@ -16,7 +18,7 @@ OUTPUT="$OUTPUT #[fg=colour202]#[bg=colour202]#[fg=colour0] $(date +'%a %d.%m
 
 # add battery
 if [[ $BATTERYLEVEL ]]; then
-	OUTPUT="$OUTPUT #[fg=colour110]#[bg=colour110]#[fg=colour0] $(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)%"
+    OUTPUT="$OUTPUT #[fg=colour110]#[bg=colour110]#[fg=colour0] $(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)%"
 fi
 
 # add hostname
